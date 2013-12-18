132fa3976e810fa40650f324d86a362c65a10314
fengsaofly <xiaoyifei1989@gmail.com>
2013-12-18 16:49:19 +0800

    主界面tab切换：时间，距离，折扣

---
+++ src/com/android/utils/GeneralFunctions.java
@@ -0,0 +1,17 @@
+package com.android.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GeneralFunctions {
+    //判断email格式是否正确
+	public static boolean isEmail(String email) {
+		String str = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+		Pattern p = Pattern.compile(str);
+		Matcher m = p.matcher(email);
+		
+		return m.matches();
+		
+	
+	}
+}
